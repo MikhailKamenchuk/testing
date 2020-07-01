9f34b4d3987e2839476ab80d1a79dce402b2df77
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-01 22:50:42 +0300

    added js lesson4 task5

---
+++ js/lesson4/task5/index.js
@@ -0,0 +1,19 @@
+const m = 4;
+const n = 14;
+let result = 0;
+for(let i = m; i <= n; i++){
+    if(i % 2 ===0 && !(i % 4 === 0) && !(i % 5 === 0)){
+        result += i;
+        continue;
+    } else if (i % 3 === 0 && !(i % 5 === 0)){
+        result -= i;
+        continue;
+    } else if(i % 4 === 0 && !(i % 5 === 0)){
+        result *= i;
+        continue;
+    } else if( i % 5 === 0){
+        console.log(i);
+    } else {
+        continue
+    }
+}
