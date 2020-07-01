3c6c2747b8a3d813933217c7b36e5d0fa79ea7ba
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-01 23:15:08 +0300

    fixed console bug in js lesson4 task8

--- js/lesson4/task8/index.js
+++ js/lesson4/task8/index.js
@@ -5,4 +5,4 @@ for (let i = 1; i <= 1000; i++) {
 let integer = Math.floor(sum / 1234);
 let rest = sum % 1234;
 
-integer > rest ? console.log('true') : console.log('false')
+console.log(integer > rest);
