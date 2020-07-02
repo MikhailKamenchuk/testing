a35d829cb5f3c5c740853386b800502f4cfcd4cb
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-02 22:50:52 +0300

    fixed bug in js lesson6 task8

--- js/lesson6/task8/index.js
+++ js/lesson6/task8/index.js
@@ -1,5 +1,5 @@
 const checkSum = (arr) => {
-    if(Array.isArray(arr)) return null;
+    if(!Array.isArray(arr)) return null;
     const sum = arr.reduce((prev, next) => prev + next, 0);
     return sum > 100
 }
