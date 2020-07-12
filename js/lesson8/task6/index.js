8716635cd5d9619f39ff3cdf85c4ed3d11340e0e
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-12 17:37:53 +0300

    added js lesson8 task6

---
+++ js/lesson8/task6/index.js
@@ -0,0 +1,19 @@
+const addPropertyV1 = (userData, userId) => {
+    userData.id = userId;
+    return userData
+}
+const addPropertyV2 = (userData, userId) => {
+    const id = {id: userId}
+    Object.assign(userData, id)
+    return userData
+}
+const addPropertyV3 = (userData, userId) => {
+    const id = {id: userId}; 
+    return Object.assign({}, userData, id)
+}
+const addPropertyV4 = (userData, userId) => {
+    return {
+      ...userData,
+      id: userId
+    }
+}
