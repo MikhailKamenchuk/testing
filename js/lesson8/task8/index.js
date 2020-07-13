76c8e99a6ca0bdc90185fea065cba84e4908eba4
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-13 14:58:17 +0300

    added js lesson8 task8

---
+++ js/lesson8/task8/index.js
@@ -0,0 +1,18 @@
+const mergeObjectsV1 = (obj1, obj2) => {
+    return Object.assign({}, obj1, obj2)
+}
+const mergeObjectsV2 = (obj1, obj2) => {
+    return Object.assign({}, obj2, obj1)
+}
+const mergeObjectsV3 = (obj1, obj2) => {
+    return {
+      ...obj1,
+      ...obj2
+    }
+}
+const mergeObjectsV4 = (obj1, obj2) => {
+    return {
+        ...obj2,
+        ...obj1
+        }
+}
