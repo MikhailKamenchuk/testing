2b4cea6a89edf0671740e97665b1c41677b84799
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-16 15:30:04 +0300

    fixed bug in js lesson10 hw1

--- js/lesson10/hw1/index.js
+++ js/lesson10/hw1/index.js
@@ -1,4 +1,4 @@
 const getTotalPrice = arr => {
-    let total = arr.reduce((a, b) => a + Math.floor(b), 0);
-    return '$' + total.toFixed(2)
+    let total = arr.reduce((a, b) => a + b, 0);
+    return '$' + Math.floor(total * 100) / 100
 }
