e5fdf079db78da6ccf1ada8954afbab9ad2925e6
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-16 15:47:45 +0300

    fixed bug in js lesson10 hw2

--- js/lesson10/hw2/index.js
+++ js/lesson10/hw2/index.js
@@ -1,7 +1,7 @@
 const getRandomNumbers = (len, start, end) => {
     if(Math.abs(end - start) < 1) return null;
     let randomNumbersArray = [];
-    for(let i = 0; i <= len; i++){
+    for(let i = 0; i < len; i++){
       let randomNumber =  Math.random() * (end - start) + start;
       randomNumber < 0 ? randomNumber = Math.ceil(randomNumber) : randomNumber = Math.floor(randomNumber); 
       randomNumbersArray.push(randomNumber);
