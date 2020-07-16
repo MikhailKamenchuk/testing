a543fcf3ebe93fd09710798454104cd30edf78a6
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-16 15:23:33 +0300

    fixed bug in js lesson10 task3

--- js/lesson10/task4/index.js
+++ js/lesson10/task4/index.js
@@ -1,5 +1,5 @@
 const getMaxAbsoluteNumber = arr => {
-    if(!Array.isArray(arr) || arr === []) return null;
+    if(!Array.isArray(arr) || arr.length === 0) return null;
     const absoluteNumbersArray = arr.map(item => Math.abs(item));
     return Math.max(...absoluteNumbersArray)
 }
