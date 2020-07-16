bd92a45ad7fd386abab370533c70368bc991e4e6
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-16 15:19:44 +0300

    added js lesson10 task4

--- js/lesson10/task4/index.js
+++ js/lesson10/task4/index.js
@@ -1,5 +1,5 @@
-
-
-
-
-
+const getMaxAbsoluteNumber = arr => {
+    if(!Array.isArray(arr)) return null;
+    const absoluteNumbersArray = arr.map(item => Math.abs(item));
+    return Math.max(...absoluteNumbersArray)
+}
