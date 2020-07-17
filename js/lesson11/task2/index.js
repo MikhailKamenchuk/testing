a086cab1c859cad8244d551e99e8f8a3b2b7ced5
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-17 11:13:10 +0300

    fixed bug in js lesson11 task2

--- js/lesson11/task2/index.js
+++ js/lesson11/task2/index.js
@@ -1,4 +1,4 @@
 const sortContacts = (arr, isAds = true) => {
     if(!Array.isArray(arr)) return null;
-    return arr.sort((a, b) => isAds ? a.name.localCompare(b.name) : b.name.localCompare(a.name))
+    return arr.sort((a, b) => isAds ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name))
 }
