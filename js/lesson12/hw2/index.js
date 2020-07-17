48c4c80bbbc4a57c34790bb911cf8186e7d0f542
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-17 15:56:43 +0300

    fixed bug in js lesson12 hw2

--- js/lesson12/hw2/index.js
+++ js/lesson12/hw2/index.js
@@ -1,8 +1,5 @@
 const cleanTransactionsList = arr => {
-    const isContainLetters = str => str.match(/[a-zA-Z]/g);
-    const copiedArr = [...arr];
-    return copiedArr 
-      .filter(item => !isContainLetters(item))
-      .map(item => String(Number(item).toFixed(2)).trim())
-      .map(item => `$${item}`);
+    return [...arr]
+      .filter(item => !isNaN(item))
+      .map(item => `$${String(Number(item).toFixed(2)).trim()}`);
 }
