bf67e0013aa7fc4ad1867be5c280d488dfe88fab
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-27 16:21:35 +0300

    fixed som bug in js lesson14 hw1

--- js/lesson14/hw1/index.js
+++ js/lesson14/hw1/index.js
@@ -1,5 +1,5 @@
 let count = 0;
-export const add = num => count + num;
-export const decrease = num => count - num;
+export const add = num => count += num;
+export const decrease = num => count -= num;
 export const reset = () => num = 0;
 export const getMemo = () => count;
