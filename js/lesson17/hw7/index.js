2afb084ad65fee45c2b2164c380d806db15e9751
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-28 15:43:24 +0300

    fixed some bug in js lesson17 hw7'

--- js/lesson17/hw7/index.js
+++ js/lesson17/hw7/index.js
@@ -1,5 +1,5 @@
 export function bind(func, context, ...args1 ){
     return function(...args2){
-        return func.apply(context, [args1, args2])
+        return func.apply(context, [...args1, ...args2])
     }
 }
