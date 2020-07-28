fa1e9cb66a522ffb63c4341ff2e58170eeb7e463
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-28 15:38:19 +0300

    fixed some bug in js lesson17 hw7'

--- js/lesson17/hw7/index.js
+++ js/lesson17/hw7/index.js
@@ -1,5 +1,6 @@
-export function bind(func, context){
-    return function(){
-        return func.apply(context, arguments)
+export function bind (context, func) {
+    const args = [].slice.call(arguments).slice(1);
+    return function() {
+        return func.apply(context, [].slice.call(arguments).concat(args));
     }
 }
