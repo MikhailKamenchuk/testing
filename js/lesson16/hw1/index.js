a6a4b62e45f7ce14cb9116522110bcae4e748829
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-28 09:24:31 +0300

    fixed some bug in js lesson16 hw1

--- js/lesson16/hw1/index.js
+++ js/lesson16/hw1/index.js
@@ -1,7 +1,7 @@
 export const createArrayOfFunctions = num => {
     let arr = [];
+    if(!num) return arr;
     if(typeof num !== 'number') return null;
-    if(!num) return [];
     for(let i = 0; i < num; i++){
         arr[i] = function(){
             return i
