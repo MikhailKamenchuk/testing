3b4e298b26f64ef0df92a3aada94834103aee762
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-29 08:32:27 +0300

    added js lesson19 task1'

---
+++ js/lesson19/task1/index.js
@@ -0,0 +1,21 @@
+export const vehicle = {
+    name: 'Agro',
+    move(){
+        console.log(`${this.name} is moving`)
+    },
+    stop(){
+        console.log(`${this.name} stopped`)
+    }
+};
+
+export const ship = {
+    startMachine(){
+        console.log(`${this.name} lifting anchor up`);
+        this.move();     
+    },
+    stopMachine(){
+        this.stop();
+        console.log(`${this.name} lifting anchor down`);     
+    }
+}
+Object.setPrototypeOf(ship, vehicle)
