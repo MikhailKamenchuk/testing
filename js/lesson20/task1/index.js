87b2cf5fba24dc9d0530eb68b850ea0999c6f069
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-29 12:59:40 +0300

    fixed some bug from js lesson20 task2'

--- js/lesson20/task1/index.js
+++ js/lesson20/task1/index.js
@@ -1,29 +1,29 @@
 export class User {
-    constructor(name, age){
+    constructor(name, age) {
         this.name = name;
         this.age = age
     }
 
-    sayHi(){
+    static createEmpty() {
+        return new User('', null)
+    }
+
+    sayHi() {
         console.log(`Hi, I am ${this.name}`)
     }
 
-    requestNewPhoto(){
+    requestNewPhoto() {
         console.log(`New photo request was sent for ${this.name}`)
     }
 
-    setAge(newAge){
-        if(newAge < 0) return false;
-        if(newAge >= 25){
+    setAge(newAge) {
+        if (newAge < 0) return false;
+        if (newAge >= 25) {
             this.age = newAge;
             this.requestNewPhoto();
-            return newAge
         }
+        return this.age
     }
 
-    static createEmpty(){
-        this.name = '';
-        this.age = null
-    }
 }
 
