49100a9ac525f434dc63e6936b3f0368c680991f
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-29 09:33:18 +0300

    added js lesson19 task4'

---
+++ js/lesson19/task4/index.js
@@ -0,0 +1,12 @@
+export const User = {
+    firstName: 'John',
+    lastName: 'Doe',
+    get fullName(){
+        return `${this.firstName} ${this.lastName}`
+    },
+    set fullName(prop){
+        const [firstName, lastName] = prop.split(' ');
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+};
