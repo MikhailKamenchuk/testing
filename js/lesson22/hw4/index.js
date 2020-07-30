a72760ab57430137a8779df669bae6e73c5aeb46
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-30 14:38:25 +0300

    added js lesson22 hw4

--- js/lesson22/hw4/index.js
+++ js/lesson22/hw4/index.js
@@ -1,5 +1,4 @@
-export const setTitle = (text) => {
-    const titleElement = document.querySelector('.title');
-    titleElement.textContent = text;
-    return titleElement
-}
+const elementInput = document.querySelector('.search_input');
+const elementBtn = document.querySelector('.search_btn');
+const handleClick = () => console.log(elementInput.value);
+elementBtn.addEventListener('click', handleClick);
