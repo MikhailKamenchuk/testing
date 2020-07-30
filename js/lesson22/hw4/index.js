83351a0d1e5815b3c9d72a73507bea0424fe82d7
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-30 14:39:41 +0300

    fixed some bug in  js lesson22 hw4

--- js/lesson22/hw4/index.js
+++ js/lesson22/hw4/index.js
@@ -1,4 +1,4 @@
-const elementInput = document.querySelector('.search_input');
-const elementBtn = document.querySelector('.search_btn');
+const elementInput = document.querySelector('.search__input');
+const elementBtn = document.querySelector('.search__btn');
 const handleClick = () => console.log(elementInput.value);
 elementBtn.addEventListener('click', handleClick);
