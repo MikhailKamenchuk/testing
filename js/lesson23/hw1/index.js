a8a5bf1e92f49ff54845d7c34d7a7c0f177f785a
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-31 17:56:42 +0300

    fixed some bug in js lesson23 hw1

--- js/lesson23/hw1/index.js
+++ js/lesson23/hw1/index.js
@@ -1,15 +1,14 @@
-let tasks = [
-    { id: Math.random(), text: 'Buy milk', done: false },
-    { id: Math.random(), text: 'Pick up Tom from airport', done: false },
-    { id: Math.random(), text: 'Visit party', done: false },
-    { id: Math.random(), text: 'Visit doctor', done: true },
-    { id: Math.random(), text: 'Buy meat', done: true },
+const tasks = [
+    { id: Math.random().toString().slice(2), text: 'Buy milk', done: false },
+    { id: Math.random().toString().slice(2), text: 'Pick up Tom from airport', done: false },
+    { id: Math.random().toString().slice(2), text: 'Visit party', done: false },
+    { id: Math.random().toString().slice(2), text: 'Visit doctor', done: true },
+    { id: Math.random().toString().slice(2), text: 'Buy meat', done: true },
 ];
 
 const listElem = document.querySelector('.list');
 
 const renderTasks = () => {
-    console.log(tasks);
     const createListItems = (id, text, isDone) => {
         const checkboxClassName = isDone ? 'list__item_done' : '';
         const isChecked = isDone ? 'checked' : '';
@@ -24,36 +23,42 @@ const renderTasks = () => {
         .sort((a, b) => a.done - b.done)
         .map(({ id, text, done }) => createListItems(id, text, done))
         .join('');
-    return listElem.innerHTML = todoListItems;
+    listElem.innerHTML = todoListItems;
 }
 renderTasks();
 
 const onStatusTodoChange = event => {
     const isCheckbox = event.target.classList.contains('list__item-checkbox');
-    if (!isCheckbox) return;
-    const currentItem = event.target.closest('.list__item');
-    const currentIndex = tasks.findIndex(item => item.id === +currentItem.dataset.id)
-    const currentTodo = tasks[currentIndex];
-    currentTodo.done = !currentTodo.done;
-    return renderTasks();
-}
-listElem.addEventListener('click', onStatusTodoChange)
+    
+    if (!isCheckbox) {
+        return;
+    };
 
-const onAddTodo = () => {
-    const createNewTodo = text => {
-        const newItem = {
-            id: Math.random(),
-            text,
-            done: false
-        };
-        tasks.push(newItem);
-        return renderTasks();
-    }
+    const currentItem = event.target.closest('.list__item');
+    const currentTask = tasks.find(item => item.id === currentItem.dataset.id);
+    currentTask.done = !currentTask.done;
+    
+    renderTasks();
+};
+
+listElem.addEventListener('click', onStatusTodoChange);
+
+const onCreateTask = () => {
     const inputTodoCreator = document.querySelector('.task-input');
-    if (inputTodoCreator.value === '') return;
-    createNewTodo(inputTodoCreator.value);
+    
+    if (inputTodoCreator.value === '') {
+        return;
+    };
+
+    const newTask = {
+        id: Math.random().toString().slice(2),
+        text: inputTodoCreator.value,
+        done: false
+    };
+    tasks.push(newTask);
     inputTodoCreator.value = '';
+    renderTasks();
 }
 
 const createTodoBtn = document.querySelector('.create-task-btn');
-createTodoBtn.addEventListener('click', onAddTodo)
+createTodoBtn.addEventListener('click', onCreateTask);
