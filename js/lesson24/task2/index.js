3191b8d0de865884d65f1afc7c00794257842adf
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-03 09:54:32 +0300

    added js lesson24 task2

--- js/lesson24/task2/index.js
+++ js/lesson24/task2/index.js
@@ -1,57 +1,10 @@
-const arenaElem = document.querySelector('.arena');
-
-const amountGenerator = amount => {
-    const arr = [];
-    for (let i = 1; i <= amount; i++) {
-        arr.push(i)
-    }
-    return arr
+export const getGreenwichTime = date => {
+    const formatter = new Intl.DateTimeFormat('en', {
+        timeZone: 'UTC',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: false
+    });
+    return formatter.format(date);
 }
-
-const getLineSeats = () => amountGenerator(10)
-    .map(seatNumber =>
-        `<div class='sector__seat' data-seat-number="${seatNumber}">
-            </div>`
-    )
-    .join('');
-
-const getSectorLines = () => {
-    const seatString = getLineSeats();
-    return amountGenerator(10)
-        .map(lineNumber =>
-            `<div class='sector__line' data-line-number="${lineNumber}">
-                ${seatString}
-            </div>`
-        )
-        .join('');
-}
-
-
-const sectorsCreator = () => {
-    const lineString = getSectorLines();
-    const sectors = amountGenerator(3)
-        .map(sectorNumber =>
-            `<div class='sector' data-sector-number="${sectorNumber}">
-                ${lineString}
-            </div>`
-        )
-        .join('');
-    return arenaElem.innerHTML = sectors;
-};
-sectorsCreator();
-
-const boardElem = document.querySelector('.board');
-
-const onSeatSelect = event => {
-    const isSeat = event.target.classList.contains('sector__seat');
-    if(!isSeat) return;
-    const seatNumber = event.target.dataset.seatNumber;
-    const lineNumber = event.target.closest('.sector__line').dataset.lineNumber;
-    const sectorNumber = event.target.closest('.sector').dataset.sectorNumber;
-    
-    const selectedSeatElem = document.querySelector('.board__selected-seat');
-    selectedSeatElem.textContent = `S ${sectorNumber} - L ${lineNumber} - S ${seatNumber}`
-}
-
-arenaElem.addEventListener('click', onSeatSelect)
 
