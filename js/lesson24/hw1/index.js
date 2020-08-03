baa606b17385515fd7084f4ca1c7fb6dea19efb8
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-03 12:21:59 +0300

    some refactoring im js lesson24 hw1

--- js/lesson24/hw1/index.js
+++ js/lesson24/hw1/index.js
@@ -9,20 +9,21 @@
 // input: two date objects
 // output: string
 
+const msInDay = 1000*60*60*24;
+const msInHour = 1000*60*60;
+const msInMin = 1000*60;
+const msInSec = 1000;
+
 export const getDiff = (startDate, endDate) => {
-    const msInDay = 1000*60*60*24;
     const diff = Math.abs(startDate.getTime() - endDate.getTime());
     const diffDays = Math.floor(diff / msInDay);
 
-    const msInHour = 1000*60*60;
     const restMsAfterGetDays = diff - diffDays * msInDay;
     const diffHours = Math.floor(restMsAfterGetDays / msInHour);
 
-    const msInMin = 1000*60;
     const restMsAfterGetHours = restMsAfterGetDays - diffHours * msInHour;
     const diffMins = Math.floor(restMsAfterGetHours / msInMin);
     
-    const msInSec = 1000;
     const restMsAfterGetMins = restMsAfterGetHours - diffMins * msInMin;
     const difSec = Math.floor(restMsAfterGetMins / msInSec);
     return `${diffDays}d ${diffHours}h ${diffMins}m ${difSec}s`;
