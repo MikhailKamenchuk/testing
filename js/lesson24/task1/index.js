9ac47532b128242b288ae50c2b234e193dc590fd
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-03 09:29:32 +0300

    fixed export ststement in  js lesson24 task1

--- js/lesson24/task1/index.js
+++ js/lesson24/task1/index.js
@@ -1,5 +1,5 @@
-export const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
-const dayOfWeek = (date, days) => {
+const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
+export const dayOfWeek = (date, days) => {
     const dayNow = new Date(date).getDate();
     const dayInFuture = new Date(date).setDate(dayNow + days);
     return weekDays[new Date(dayInFuture).getDate()];
