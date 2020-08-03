8b8ba1242080414ba1fdc4068f325cec0200e08c
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-03 13:15:40 +0300

    added js lesson24 hw2

--- js/lesson24/hw2/index.js
+++ js/lesson24/hw2/index.js
@@ -1,16 +1,48 @@
 const tasks = [
-    { id: Math.random().toString().slice(2), text: 'Buy milk', done: false },
-    { id: Math.random().toString().slice(2), text: 'Pick up Tom from airport', done: false },
-    { id: Math.random().toString().slice(2), text: 'Visit party', done: false },
-    { id: Math.random().toString().slice(2), text: 'Visit doctor', done: true },
-    { id: Math.random().toString().slice(2), text: 'Buy meat', done: true },
+    { 
+        id: Math.random().toString().slice(2), 
+        text: 'Buy milk', 
+        done: false, 
+        createDate: new Date(2020, 1, 1, 11, 10, 10),
+        finishedDay: null,
+    },
+    { 
+        id: Math.random().toString().slice(2), 
+        text: 'Pick up Tom from airport', 
+        done: false,
+        createDate: new Date(2020, 1, 2, 11, 10, 10),
+        finishedDay: null,
+    },
+    { 
+        id: Math.random().toString().slice(2), 
+        text: 'Visit party', 
+        done: false ,
+        createDate: new Date(2020, 1, 3, 11, 10, 10),
+        finishedDay: null,
+    },
+    { 
+        id: Math.random().toString().slice(2), 
+        text: 'Visit doctor', 
+        done: true,
+        createDate: new Date(2020, 1, 4, 11, 10, 10),
+        finishedDay: new Date(2020, 7, 4, 11, 10, 10),
+    },
+    { 
+        id: Math.random().toString().slice(2), 
+        text: 'Buy meat', 
+        done: true,
+        createDate: new Date(2020, 1, 2, 11, 10, 10),
+        finishedDay: new Date(2020, 7, 5, 11, 10, 10),
+     },
 ];
 
 const listElem = document.querySelector('.list');
 
 const renderTasks = () => {
     const todoListItems = tasks
-        .sort((a, b) => a.done - b.done)
+    .sort((a, b) => b.createDate - a.createDate)
+    .sort((a, b) => a.done - b.done)
+    .sort((a, b) => a.finishedDay - b.finishedDay)
         .map(({ id, text, done }) => {
             const newTaskItem = document.createElement('li');
             newTaskItem.classList.add(`list__item`);
@@ -30,6 +62,7 @@ const renderTasks = () => {
 
         listElem.innerHTML = '';
         listElem.append(...todoListItems);
+        console.log(tasks)
 }
 renderTasks();
 
@@ -43,6 +76,8 @@ const onStatusTodoChange = event => {
     const currentItem = event.target.closest('.list__item');
     const currentTask = tasks.find(item => item.id === currentItem.dataset.id);
     currentTask.done = !currentTask.done;
+    !currentTask.finishedDay ? currentTask.finishedDay = new Date() : currentTask.finishedDay = null;
+    !currentTask.createDate ? currentTask.createDate = new Date() : currentTask.createDate = null;
     
     renderTasks();
 };
@@ -59,7 +94,9 @@ const onCreateTask = () => {
     const newTask = {
         id: Math.random().toString().slice(2),
         text: inputTodoCreator.value,
-        done: false
+        done: false,
+        createDate: new Date(),
+        finishedDay: null,
     };
     tasks.push(newTask);
     inputTodoCreator.value = '';
