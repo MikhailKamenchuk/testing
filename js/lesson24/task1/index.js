943cd1c0f87129983662a0b9027adc1f977f7a10
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-03 09:40:08 +0300

    fixed bug in  js lesson24 task1

--- js/lesson24/task1/index.js
+++ js/lesson24/task1/index.js
@@ -1,6 +1,6 @@
-const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
-export const dayOfWeek = (date, days) => {
-    const dayNow = new Date(date).getDate();
+ export const dayOfWeek = (date, days) => {
+    const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
+    const dayNow = new Date(date).getDay();
     const dayInFuture = new Date(date).setDate(dayNow + days);
     return weekDays[new Date(dayInFuture).getDate()];
-}
+};
