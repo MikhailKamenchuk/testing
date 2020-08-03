ef4fa13d00756a31bb21bd1185c5f70e3aa93a8e
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-03 12:07:16 +0300

    added algo in js lesson24 hw1

--- js/lesson24/hw1/index.js
+++ js/lesson24/hw1/index.js
@@ -1,3 +1,14 @@
+// algo:
+// 1. find the difference between two dates modulo in ms;
+// 2. get amount days from this amount ms and get the rest ms after this operation;
+// 3. get amount hours from the rest ms after previous operation and get the rest ms after this operation;
+// 3. get amount min from the rest ms after previous operation and get the rest ms after this operation;
+// 4. get amount sec from the rest ms after previous operation;
+// 5. return this string like '2d 23h 34m 32s';
+
+// input: two date objects
+// output: string
+
 export const getDiff = (startDate, endDate) => {
     const msInDay = 1000*60*60*24;
     const diff = Math.abs(startDate.getTime() - endDate.getTime());
