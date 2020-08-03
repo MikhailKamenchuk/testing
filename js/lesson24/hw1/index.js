a78af137fb129dbdd54b87acc5cc4d9b6467688e
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-03 11:56:43 +0300

    added js lesson24 hw11

--- js/lesson24/hw1/index.js
+++ js/lesson24/hw1/index.js
@@ -1,70 +1,18 @@
-const tasks = [
-    { id: Math.random().toString().slice(2), text: 'Buy milk', done: false },
-    { id: Math.random().toString().slice(2), text: 'Pick up Tom from airport', done: false },
-    { id: Math.random().toString().slice(2), text: 'Visit party', done: false },
-    { id: Math.random().toString().slice(2), text: 'Visit doctor', done: true },
-    { id: Math.random().toString().slice(2), text: 'Buy meat', done: true },
-];
+export const getDiff = (startDate, endDate) => {
+    const msInDay = 1000*60*60*24;
+    const diff = Math.abs(startDate.getTime() - endDate.getTime());
+    const diffDays = Math.floor(diff / msInDay);
 
-const listElem = document.querySelector('.list');
+    const msInHour = 1000*60*60;
+    const restMsAfterGetDays = diff - diffDays * msInDay;
+    const diffHours = Math.floor(restMsAfterGetDays / msInHour);
 
-const renderTasks = () => {
-    const todoListItems = tasks
-        .sort((a, b) => a.done - b.done)
-        .map(({ id, text, done }) => {
-            const newTaskItem = document.createElement('li');
-            newTaskItem.classList.add(`list__item`);
-            newTaskItem.dataset.id = id;
-            if (done) {
-                newTaskItem.classList.add('list__item_done');
-            }
-            
-            const newCheckboxItem = document.createElement('input');
-            newCheckboxItem.classList.add('list__item-checkbox');
-            newCheckboxItem.setAttribute('type', 'checkbox');
-            newCheckboxItem.checked = done;
-            
-            newTaskItem.append(newCheckboxItem, text); 
-            return newTaskItem;
-        });
-
-        listElem.innerHTML = '';
-        listElem.append(...todoListItems);
+    const msInMin = 1000*60;
+    const restMsAfterGetHours = restMsAfterGetDays - diffHours * msInHour;
+    const diffMins = Math.floor(restMsAfterGetHours / msInMin);
+    
+    const msInSec = 1000;
+    const restMsAfterGetMins = restMsAfterGetHours - diffMins * msInMin;
+    const difSec = Math.floor(restMsAfterGetMins / msInSec);
+    return `${diffDays}d ${diffHours}h ${diffMins}m ${difSec}s`;
 }
-renderTasks();
-
-const onStatusTodoChange = event => {
-    const isCheckbox = event.target.classList.contains('list__item-checkbox');
-    
-    if (!isCheckbox) {
-        return;
-    };
-
-    const currentItem = event.target.closest('.list__item');
-    const currentTask = tasks.find(item => item.id === currentItem.dataset.id);
-    currentTask.done = !currentTask.done;
-    
-    renderTasks();
-};
-
-listElem.addEventListener('click', onStatusTodoChange);
-
-const onCreateTask = () => {
-    const inputTodoCreator = document.querySelector('.task-input');
-    
-    if (inputTodoCreator.value === '') {
-        return;
-    };
-
-    const newTask = {
-        id: Math.random().toString().slice(2),
-        text: inputTodoCreator.value,
-        done: false
-    };
-    tasks.push(newTask);
-    inputTodoCreator.value = '';
-    renderTasks();
-}
-
-const createTodoBtn = document.querySelector('.create-task-btn');
-createTodoBtn.addEventListener('click', onCreateTask);
