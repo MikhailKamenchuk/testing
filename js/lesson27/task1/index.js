549ebb848a20846ae9917949715baa8c5128c2e5
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-04 13:40:42 +0300

    fixed some bug in lesson27 task1

--- js/lesson27/task1/index.js
+++ js/lesson27/task1/index.js
@@ -1,17 +1,14 @@
-localStorage.setItem('num',  JSON.stringify(23));
-localStorage.setItem('arr',  JSON.stringify([1, 'qwd']));
-localStorage.setItem('str',  JSON.stringify('846'));
-
 const getLocalStorageData = () => {
     return Object.entries(localStorage)
-        .reduce((acc, [key, value]) => {
-           acc[key] = JSON.parse(value);
-           return acc
+        .reduce((acc, [key, value]) => {  
+           return {
+            ...acc,
+            [key]: JSON.parse(value)
+           }
         }, {})
 }
 
 export{
     getLocalStorageData
 }
-// console.log(getLocalStorageData())
    
