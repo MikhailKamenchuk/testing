ab9bd1b8d82d1ebd7cf3584ec0a078c905a1d3dc
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-05 14:11:03 +0300

    refactored js lesson28 hw3

--- js/lesson28/hw3/index.js
+++ js/lesson28/hw3/index.js
@@ -5,6 +5,4 @@
 // 1.function for input <= 1 must returned  input
 // 2.for rest => must returned last number + (last number - 1)
 
-export const sum = n => {
-    return n <= 1 ? n : n + sum(n - 1)
-}
+export const sum = n => n <= 1 ? n : n + sum(n - 1)
