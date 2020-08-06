004c23bb3be4fa4cf303d27493d0ac12902d3169
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-06 15:24:19 +0300

    some refactoring js lesson29 hw1

--- js/lesson29/hw1/index.js
+++ js/lesson29/hw1/index.js
@@ -1,17 +1,5 @@
 export function delay(del, callback, context, ...args){
-    
      setTimeout(() => {
-        // console.log(del)
          return callback.apply(context, args)
     }, del);
 }
-// const obj = {
-//     z: 10,
-//     func(a, b, c) {
-//         console.log( a + b + c + this.z)
-//     },
-// }
-
-// // console.log(obj.func(1,2,3))
-
-// console.log(delay(500,  obj.func, obj, 1, 2, 3))
