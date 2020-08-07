8357c351056917118c491c03bde2b6b1d08b2b0b
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-07 10:13:41 +0300

    some refactoring in js lesson29 hw2

--- js/lesson29/hw2/index.js
+++ js/lesson29/hw2/index.js
@@ -7,26 +7,14 @@
 // return callback with data from delay
 
 export function requestUserData(userId, callback){
-    const user = {
-        userId,
-        email: `${userId}@example.com`,
-    }
     const randomDelay = Math.floor((Math.random() * 3) + 1) * 1000;
     
     if( userId === 'broken' ){
         setTimeout(() => callback(null, 'Failed to load user data'), randomDelay);
     }else{
-        setTimeout(() => callback(user), randomDelay);
+        setTimeout(() => callback({
+            userId,
+            email: `${userId}@example.com`,
+        }), randomDelay);
     }
 }
-
-// const onUserDataRequested = (obj, error) => {
-//     if (error) {
-//         console.log(error);
-//         return;
-//     };
-    
-//     console.log(obj)
-// }
-// requestUserData('id-12345678', onUserDataRequested)
-// requestUserData('broken', onUserDataRequested)
