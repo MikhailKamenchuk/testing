38b2b27123c97effe2ec7fcc30578d9f6eebc2ba
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-11 16:16:54 +0300

    added js lesson32 hw1

---
+++ js/lesson32/hw1/index.js
@@ -0,0 +1,14 @@
+/*
+ * ф-ция makePromise должна вернуть промис со значением переданным в ф-цию
+ */
+
+/* ...code here */
+export const makePromise = value => Promise.resolve(value);
+
+/*
+ * пример использования
+ */
+makePromise(17)
+    .then(number => {
+        console.log(number); // 17
+    });
