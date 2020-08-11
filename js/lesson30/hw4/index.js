dc0683ccef33f68028c2bdf44f3f8cfbca0a968a
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-11 08:00:19 +0300

    added js lesson 30 hw4

---
+++ js/lesson30/hw4/index.js
@@ -0,0 +1,33 @@
+export const addImage = (url, callback) => {
+    const img = document.createElement('img');
+    img.setAttribute('alt', 'User avatar');
+    img.src = url;
+
+    const pageElem = document.querySelector('.page');
+    pageElem.append(img);
+    
+    const onImageLoaded = () => {
+        const { width, height } = img;
+        callback(null, { width, height });
+    }
+    
+    const onImageLoadError = () => callback('Image load failed');
+    
+    img.addEventListener('load', onImageLoaded);
+    
+    img.addEventListener('error', onImageLoadError);
+};
+
+export const addImageV2 = imgSrc => {
+    const p = new Promise((resolve, reject) => {
+        addImage(imgSrc, (err, script) => {
+            err ? reject(err) : resolve(script)
+        })
+    });
+    return p
+}
+
+// test call
+// addImageV2("https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg")
+// .then(data => console.log(data))
+// .catch(error => console.log(error));
