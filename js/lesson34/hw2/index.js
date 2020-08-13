a3736da58629a1e281b7e786cbcd7507c4286257
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-13 14:09:40 +0300

    added js lesson34 hw2

--- js/lesson34/hw2/index.js
+++ js/lesson34/hw2/index.js
@@ -1,15 +1,34 @@
 const loginFormElem = document.querySelector('.login-form');
-// submit-button
+const submitFormBtnElem = document.querySelector('.submit-button');
 const errorTextElem = document.querySelector('.error-text');
-const baseUrl = 'https://5f33e64ccfaf5a001646b6d1.mockapi.io/api/v1/users';
+const baseUrl = 'https://5f33e64ccfaf5a001646b6d1.mockapi.io/api/v1/users2';
 
-const fetchRequest = (loginData) => fetch(baseUrl, {
+const fetchRequest = loginData => fetch(baseUrl, {
     method: 'POST',
-    headers: 'Content-Type: application/json; charset=UTF-8',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+    },
     body: JSON.stringify(loginData)
 })
 
-const onSendLoginData = e => {
-    loginFormElem.reportValidity();
+const onFormFieldsChange = () => {
+    errorTextElem.textContent = null;
+
+    if (loginFormElem.reportValidity()) {
+        submitFormBtnElem.disabled = false
+    }
 }
-loginFormElem.addEventListener('submit', onSendLoginData)
+loginFormElem.addEventListener('input', onFormFieldsChange);
+
+const onSendLoginData = e => {
+    e.preventDefault();
+    const formData = Object.fromEntries(new FormData(e.target).entries());
+    console.log(formData);
+    fetchRequest(formData)
+        .then(res => res.json())
+        .then(data => alert(JSON.stringify(data)))
+        .catch(() => {
+            errorTextElem.textContent = 'Failed to create user' 
+        })
+}
+loginFormElem.addEventListener('submit', onSendLoginData);
