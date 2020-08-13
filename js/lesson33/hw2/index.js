b1fe6168cee7e8e3a238623341810cbd22d0a25d
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-13 18:00:21 +0300

    some refactoring in js lesson33 hw2

--- js/lesson33/hw2/index.js
+++ js/lesson33/hw2/index.js
@@ -1,23 +1,26 @@
 const getAllDevs = (userId, repoId) => fetch(`https://api.github.com/repos/${userId}/${repoId}/commits?per_page=100`)
         .then(res => res.json());
 
-export const getMostActiveDevs = ({ days, userId, repoId }) => {
+const getMostActiveDevs = ({ days, userId, repoId }) => {
     return getAllDevs(userId, repoId).then(data => data
-        .filter(item => {
-            const { commit: { author: { date } } } = item;
+        .map(({ commit: { author: { name, email, date } } }) => ({ 
+            name, 
+            email, 
+            date, 
+        }))
+        .filter(({ date }) => {
             const maxPossibleDate = new Date();
             maxPossibleDate.setDate(maxPossibleDate.getDate() - days);
             return (new Date(date) > new Date(maxPossibleDate))
         })
-        .map(item => {
-            const { commit: { author: { name, email } } } = item;
-            return { name, email }
-        })
         .reduce((acc, item) => {
+            delete item.date;
+
             if (acc.length === 0 || !acc.some(el => el.name === item.name)) {
                 acc.push(Object.assign(item, { count: 1 }))
                 return acc
             };
+            
             acc.map(el => el.name === item.name
                 ? el.count += 1
                 : { ...el }
@@ -29,4 +32,4 @@ export const getMostActiveDevs = ({ days, userId, repoId }) => {
 }
     
 
-// getMostActiveDevs({ days: 7, userId: 'MikhailKamenchuk', repoId: 'live-session' }).then(res => console.log(res))
+getMostActiveDevs({ days: 7, userId: 'MikhailKamenchuk', repoId: 'live-session' }).then(res => console.log(res))
