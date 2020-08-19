8afcff906e4ec143ffde3581d2fd2fafafe0fd01
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-19 18:19:17 +0300

    some refactoring in react lesson1 task3

--- react/lesson1/task3/webpack.config.js
+++ react/lesson1/task3/webpack.config.js
@@ -1,6 +1,11 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require('path');
 
 module.exports = {
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'review_build')
+  },
   module: {
     rules: [
       {
